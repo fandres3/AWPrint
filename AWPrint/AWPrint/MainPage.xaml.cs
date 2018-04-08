ca2a509d0c3daeaf2642232599331530e4531e5f
fandres <fandres@gmail.com>
2018-04-08 15:53:39 +0200

    Agregar archivos de proyecto.

---
+++ AWPrint/AWPrint/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AWPrint
+{
+	public partial class MainPage : MasterDetailPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            Detail = new NavigationPage(new Page1());
+            IsPresented = false;
+        }
+
+        void BtnImprimirClicked(object sender, System.EventArgs e)
+        {
+            Detail = new NavigationPage(new Page1());
+            IsPresented = false;
+        }
+
+        void BtnAjustesClicked(object sender, System.EventArgs e)
+        {
+            Detail = new NavigationPage(new Page2());
+            IsPresented = false;
+        }
+    }
+}
